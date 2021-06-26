f733a2c3cf91fe4e291486ac4937c6d532006d47
JinnyK015 <79676812+JinnyK015@users.noreply.github.com>
2021-06-26 23:04:43 +1200

    navber-menu scroll js

--- main.js
+++ main.js
@@ -2,6 +2,19 @@
 // navber click scrolling 
 const nav_menu = document.querySelector('.navbar__menu');
 
+nav_menu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+})
+
+//navbar fixed and trans
+
 
 // Home typing text function part
 
